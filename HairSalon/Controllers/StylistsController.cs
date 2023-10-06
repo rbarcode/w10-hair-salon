503412e041fc848e27faa34a672d04e7c7cc4a0f
Richard Barbour <richyb07@gmail.com>
2023-10-06 16:27:23 -0700

    Refactor Stylist class controller and views to show formatted phone number from unformatted database data.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -39,6 +39,10 @@ namespace HairSalon.Controllers
       Stylist thisStylist = _db.Stylists
                                 .Include(stylists => stylists.Clients)
                                 .FirstOrDefault(stylists => stylists.StylistId == id);
+      string areaCode = thisStylist.PhoneNumber.Substring(0, 3);
+      string telPrefix = thisStylist.PhoneNumber.Substring(3, 3);
+      string lineNum = thisStylist.PhoneNumber.Substring(6);
+      ViewBag.FormattedPhoneNum = $"({areaCode}) {telPrefix}-{lineNum}";
       return View(thisStylist);
     }
   }
