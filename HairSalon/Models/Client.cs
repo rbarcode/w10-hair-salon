88277f0974fdd6d0f2066b6fcbd4238a38f5b883
Richard Barbour <richyb07@gmail.com>
2023-10-06 11:27:42 -0700

    Update PhoneNumber property in Client.cs to type string to match varchar type in MySQL.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,7 +4,7 @@ namespace HairSalon.Models
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
-    public int PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; }
     public string EmailAddress { get; set; }
     public int StylistId { get; set; }
     public Stylist Stylist { get; set; }
